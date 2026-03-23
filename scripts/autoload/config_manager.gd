b6a0d1d045839b8c7ebb5ff5ed1129c48536f3b2
KaschaArtem <kascha767611@gmail.com>
2026-03-23 22:15:23 +0300

    code refactor for config manager

--- scripts/autoload/config_manager.gd
+++ scripts/autoload/config_manager.gd
@@ -16,9 +16,9 @@ var RESOLUTIONS = [
 
 var valid_resolutions: Array = []
 
-var cur_main_bus_value: int
-var cur_music_bus_value: int
-var cur_effects_bus_value: int
+var cur_main_bus_value: int = 10
+var cur_music_bus_value: int = 10
+var cur_effects_bus_value: int = 10
 
 
 func calc_valid_resolutions():
@@ -26,13 +26,27 @@ func calc_valid_resolutions():
 	for res in RESOLUTIONS:
 		if res.x <= screen_size.x and res.y <= screen_size.y:
 			valid_resolutions.append(res)
-
 func center_window():
+	if DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_WINDOWED:
+		return
 	var screen_size = DisplayServer.screen_get_size()
 	var window_size = DisplayServer.window_get_size()
 	var new_position = (screen_size - window_size) / 2
 	DisplayServer.window_set_position(new_position)
 	get_tree().root.size = DisplayServer.window_get_size()
+
+func load_config() -> bool:
+	var config = ConfigFile.new()
+	var err = config.load(CONFIG_PATH)
+	if err != OK:
+		return false
+	DisplayServer.window_set_size(config.get_value("display", "resolution", Vector2i(1152, 648)))
+	DisplayServer.window_set_mode(config.get_value("display", "mode", DisplayServer.WINDOW_MODE_WINDOWED))
+	center_window()
+	update_main_bus(config.get_value("audio", "main", 10))
+	update_music_bus(config.get_value("audio", "music", 10))
+	update_effects_bus(config.get_value("audio", "effects", 10))
+	return true
 
 func save_config() -> void:
 	var config = ConfigFile.new()
@@ -45,58 +59,28 @@ func save_config() -> void:
 	if err != OK:
 		push_error("Failed to save config")
 
-func apply_best_resolution():
-	if valid_resolutions.size() == 0:
-		DisplayServer.window_set_size(DisplayServer.screen_get_size())
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-		return
-	DisplayServer.window_set_size(valid_resolutions[-1])
-	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-	get_tree().root.size = DisplayServer.window_get_size()
-
-func apply_loaded_display_settings(resolution: Vector2i, mode: int):
-	if resolution not in valid_resolutions:
-		apply_best_resolution()
-	else:
-		DisplayServer.window_set_size(resolution)
-		get_tree().root.size = resolution
-	DisplayServer.window_set_mode(mode)
-	if mode == DisplayServer.WINDOW_MODE_WINDOWED:
-		center_window()
-
-func load_config() -> bool:
-	var config = ConfigFile.new()
-	var err = config.load(CONFIG_PATH)
-	if err != OK:
-		return false
-	var resolution = config.get_value("display", "resolution", Vector2i(1152, 648))
-	var mode = config.get_value("display", "mode", DisplayServer.WINDOW_MODE_FULLSCREEN)
-	apply_loaded_display_settings(resolution, mode)
-	update_main_bus(config.get_value("audio", "main", 10))
-	update_music_bus(config.get_value("audio", "music", 10))
-	update_effects_bus(config.get_value("audio", "effects", 10))
-	return true
-
 func _ready() -> void:
 	calc_valid_resolutions()
 	if load_config():
 		pass
 	else:
-		apply_best_resolution()
 		save_config()
 
 
 func toggle_display_mode():
 	var mode = DisplayServer.window_get_mode()
-	if mode == DisplayServer.WINDOW_MODE_WINDOWED:
-		DisplayServer.window_set_size(valid_resolutions[-1])
-		get_tree().root.size = DisplayServer.window_get_size()
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-	else:
-		DisplayServer.window_set_size(valid_resolutions[-1])
-		get_tree().root.size = DisplayServer.window_get_size()
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-		center_window()
+	match mode:
+		DisplayServer.WINDOW_MODE_WINDOWED:
+			DisplayServer.window_set_size(valid_resolutions[-1])
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		DisplayServer.WINDOW_MODE_FULLSCREEN:
+			DisplayServer.window_set_size(valid_resolutions[-1])
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			center_window()
+		_:
+			DisplayServer.window_set_size(valid_resolutions[-1])
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			center_window()
 
 func decrease_resolution():
 	var current = DisplayServer.window_get_size()
@@ -105,36 +89,25 @@ func decrease_resolution():
 		index = valid_resolutions.size() - 1
 	index = (index - 1 + valid_resolutions.size()) % valid_resolutions.size()
 	DisplayServer.window_set_size(valid_resolutions[index])
-	get_tree().root.size = DisplayServer.window_get_size()
 	center_window()
 
 
-func update_main_bus(value: int) -> void:
-	cur_main_bus_value = value
-	var bus_index = AudioServer.get_bus_index("Master")
+func update_bus(value: int, bus_index: int) -> void:
 	if value == 0:
 		AudioServer.set_bus_mute(bus_index, true)
 	else:
 		AudioServer.set_bus_mute(bus_index, false)
 		var db = linear_to_db(value / 10.0)
 		AudioServer.set_bus_volume_db(bus_index, db)
+
+func update_main_bus(value: int) -> void:
+	cur_main_bus_value = value
+	update_bus(value, AudioServer.get_bus_index("Master"))
 
 func update_music_bus(value: int) -> void:
 	cur_music_bus_value = value
-	var bus_index = AudioServer.get_bus_index("Music")
-	if value == 0:
-		AudioServer.set_bus_mute(bus_index, true)
-	else:
-		AudioServer.set_bus_mute(bus_index, false)
-		var db = linear_to_db(value / 10.0)
-		AudioServer.set_bus_volume_db(bus_index, db)
+	update_bus(value, AudioServer.get_bus_index("Music"))
 
 func update_effects_bus(value: int) -> void:
 	cur_effects_bus_value = value
-	var bus_index = AudioServer.get_bus_index("Effects")
-	if value == 0:
-		AudioServer.set_bus_mute(bus_index, true)
-	else:
-		AudioServer.set_bus_mute(bus_index, false)
-		var db = linear_to_db(value / 10.0)
-		AudioServer.set_bus_volume_db(bus_index, db)
+	update_bus(value, AudioServer.get_bus_index("Effects"))
--- scripts/menu/settings.gd
+++ scripts/menu/settings.gd
@@ -24,7 +24,19 @@ func update_display_mode() -> void:
 			display_mode_label.text = "Windowed"
 		DisplayServer.WINDOW_MODE_FULLSCREEN:
 			display_mode_label.text = "Borderless"
-func update_display_resolution(resolution) -> void:
+		_:
+			display_mode_label.text = "Unknown"
+func toggle_display_resolution_button() -> void:
+	var mode = DisplayServer.window_get_mode()
+	match mode:
+		DisplayServer.WINDOW_MODE_WINDOWED:
+			display_resolution_button.disabled = false
+		DisplayServer.WINDOW_MODE_FULLSCREEN:
+			display_resolution_button.disabled = true
+		_:
+			display_resolution_button.disabled = false
+func _update_display_resolution() -> void:
+	var resolution = DisplayServer.window_get_size()
 	dislpay_resolution_label.text = str(resolution.x) + "x" + str(resolution.y)
 func update_main_value() -> void:
 	main_value.text = str(int(main_slider.value))
@@ -32,36 +44,20 @@ func update_music_value() -> void:
 	music_value.text = str(int(music_slider.value))
 func update_effects_value() -> void:
 	effects_value.text = str(int(effects_slider.value))
-func update_main_slider() -> void:
+func init_sliders_values() -> void:
 	main_slider.value = ConfigManager.cur_main_bus_value
-func update_music_slider() -> void:
 	music_slider.value = ConfigManager.cur_music_bus_value
-func update_effects_slider() -> void:
 	effects_slider.value = ConfigManager.cur_effects_bus_value
+
 
 func _ready() -> void:
 	update_display_mode()
 	toggle_display_resolution_button()
-	update_main_slider()
-	update_music_slider()
-	update_effects_slider()
+	get_viewport().size_changed.connect(_update_display_resolution)
+	_update_display_resolution()
+	init_sliders_values()
 	self.visible = false
 
-
-func _on_back_pressed() -> void:
-	SFXManager.play_sound("menu_nav_button")
-	menu.update(main)
-	ConfigManager.save_config()
-
-
-func toggle_display_resolution_button() -> void:
-	var mode = DisplayServer.window_get_mode()
-	if mode == DisplayServer.WINDOW_MODE_WINDOWED:
-		update_display_resolution(DisplayServer.window_get_size())
-		display_resolution_button.disabled = false
-	else:
-		update_display_resolution(DisplayServer.screen_get_size())
-		display_resolution_button.disabled = true
 
 func _on_display_mode_pressed() -> void:
 	SFXManager.play_sound("menu_click_button")
@@ -72,7 +68,6 @@ func _on_display_mode_pressed() -> void:
 func _on_display_resolution_pressed() -> void:
 	SFXManager.play_sound("menu_click_button")
 	ConfigManager.decrease_resolution()
-	update_display_resolution(DisplayServer.window_get_size())
 
 
 func _on_main_slider_value_changed(value: float) -> void:
@@ -86,6 +81,12 @@ func _on_music_slider_value_changed(value: float) -> void:
 func _on_effects_slider_value_changed(value: float) -> void:
 	ConfigManager.update_effects_bus(int(value))
 	update_effects_value()
+
+
+func _on_back_pressed() -> void:
+	SFXManager.play_sound("menu_nav_button")
+	menu.update(main)
+	ConfigManager.save_config()
 
 
 func _input(_event) -> void:
