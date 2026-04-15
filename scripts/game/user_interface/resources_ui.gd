5dd7d8d5a6a6acbc4b45de404695983510be56e0
KaschaArtem <kascha767611@gmail.com>
2026-04-15 11:19:28 +0300

    added resources info

--- scripts/game/user_interface/resources_ui.gd
+++ scripts/game/user_interface/resources_ui.gd
@@ -3,6 +3,13 @@ extends Control
 
 @export var game: Node3D
 
+
+const RESOURCES_INFO = {
+	"people": "You can allocate PEOPLE to do certain work. The more PEOPLE you allocate at one work, the more efficient this work will be.",
+	"wood": "Resource for build. On winter will be used to warm up PEOPLE.",
+	"food": "Resource for PEOPLE's sustenance. If not enough, PEOPLE will die."
+}
+
 @export var actual_people: Label
 @export var max_people: Label
 @export var actual_wood: Label
@@ -11,10 +18,23 @@ extends Control
 @export var actual_animal_food: Label
 @export var max_food: Label
 
+@export var people_info: Control
+@export var wood_info: Control
+@export var food_info: Control
 
-func _ready() -> void:
-	game.resources_changed.connect(update_resources)
-	update_resources()
+@export var people_info_label: Label
+@export var wood_info_label: Label
+@export var food_info_label: Label
+
+
+func setup_info_tabs() -> void:
+	people_info_label.text = RESOURCES_INFO["people"]
+	wood_info_label.text = RESOURCES_INFO["wood"]
+	food_info_label.text = RESOURCES_INFO["food"]
+
+	people_info.visible = false
+	wood_info.visible = false
+	food_info.visible = false
 
 func update_resources() -> void:
 	actual_people.text = str(game.human_resource)
@@ -24,3 +44,27 @@ func update_resources() -> void:
 	actual_plant_food.text = str(game.plant_food_resource)
 	actual_animal_food.text = str(game.animal_food_resource)
 	max_food.text = str(game.max_food_resource)
+
+func _ready() -> void:
+	game.resources_changed.connect(update_resources)
+	update_resources()
+	setup_info_tabs()
+
+
+func _on_people_mouse_entered() -> void:
+	people_info.visible = true
+
+func _on_people_mouse_exited() -> void:
+	people_info.visible = false
+
+func _on_wood_mouse_entered() -> void:
+	wood_info.visible = true
+
+func _on_wood_mouse_exited() -> void:
+	wood_info.visible = false
+
+func _on_food_mouse_entered() -> void:
+	food_info.visible = true
+
+func _on_food_mouse_exited() -> void:
+	food_info.visible = false
